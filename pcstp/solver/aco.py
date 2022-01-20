c357c14389abca522166b889277490340ccd8199
Vinicius Mello <viniciusdsmello@poli.ufrj.br>
2022-01-20 17:36:21 -0300

    Adds aco solve method

--- pcstp/solver/aco.py
+++ pcstp/solver/aco.py
@@ -1,10 +1,13 @@
 """Module that implements Ant Colony Optmization Solver for Prize-Collecting Steiner Tree Problem"""
 
 import random
+import time
 from typing import List, Tuple
-from charset_normalizer import logging
 
 import networkx as nx
+import networkx.algorithms.components as comp
+import numpy as np
+from charset_normalizer import logging
 from pcstp.solver.base import BaseSolver
 
 
@@ -20,26 +23,35 @@ class AntColony(BaseSolver):
             beta: float = 0.0,
             beta_evaporation_rate: float = 0,
             initial_pheromone: float = 1,
-            pheromone_per_iteration: float = 1,
+            pheromone_amount: float = 1,
             pheromone_initialization_strategy: str = 'same',
+            choose_best: float = 0.1,
+            early_stopping: int = None,
             **kwargs):
         """
         Prize Collecting Steiner Tree Solver based on Ant Colony Optmization 
 
         Args:
-            graph (nx.Graph): [description]
-            terminals (set): [description]
-            iterations (int, optional): [description]. Defaults to 10.
-            num_ants (int, optional): [description]. Defaults to 10.
-            evaporation_rate (float, optional): [description]. Defaults to 0.1.
+            graph (nx.Graph): Instance Graph
+            terminals (set): List of Terminals
+            iterations (int, optional): Number of iterations. Defaults to 10.
+            num_ants (int, optional): Number of ants. Defaults to 10.
+            evaporation_rate (float, optional): Pheromone evaporation rate. Defaults to 0.1.
             alpha (float, optional): [description]. Defaults to 1.0.
             beta (float, optional): [description]. Defaults to 0.0.
-            beta_evaporation_rate (float, optional): [description]. Defaults to 0.
-            initial_pheromone (float, optional): [description]. Defaults to 1.
-            pheromone_per_iteration (float, optional): [description]. Defaults to 1.
-            pheromone_initialization_strategy (str, optional): [description]. Defaults to 'same'.
+            beta_evaporation_rate (float, optional): Beta evaporation rate. Defaults to 0.
+            initial_pheromone (float, optional): Initial pheromone value. Defaults to 1.
+            pheromone_amount (float, optional): Pheromone amout deposited by each ant. Defaults to 1.
+            pheromone_initialization_strategy (str, optional): Pheromone initialization strategy.
+                When 'same' is passed, all edges are initialized with `initial_pheromone`.
+                When 'heuristic' is passed, a greedy heuristic is used to initialize the edges with initial pheromone.
+                Defaults to 'same'.
+            choose_best (float, optional): Indicates at which probability the best path will be take by each ant. Defaults to 0.1.
         """
         super().__init__(graph, terminals, **kwargs)
+
+        assert evaporation_rate > 0, 'evaporation_rate must be greater than 0'
+        assert evaporation_rate <= 1, 'evaporation_rate must be less or equal than 1'
 
         self.iterations = iterations
         self.num_ants = num_ants
@@ -48,8 +60,14 @@ class AntColony(BaseSolver):
         self.beta = beta
         self.beta_evaporation_rate = beta_evaporation_rate
         self.initial_pheromone = initial_pheromone
-        self.pheromone_per_iteration = pheromone_per_iteration
+        self.pheromone_amount = pheromone_amount
         self.pheromone_initialization_strategy = pheromone_initialization_strategy
+        self.choose_best = choose_best
+
+        if early_stopping:
+            self.early_stopping = early_stopping
+        else:
+            self.early_stopping = self.iterations
 
         self.ants: List[Ant] = []
 
@@ -64,26 +82,26 @@ class AntColony(BaseSolver):
             Ant(self, ant) for ant in range(self.num_ants)
         ]
 
-        logging.debug("Initializing pheromones...")
+        logging.debug("Initializing pheromones (strategy %s)...", self.pheromone_initialization_strategy)
 
         if self.pheromone_initialization_strategy == 'same':
             initial_pheromone = self.initial_pheromone
         elif self.pheromone_initialization_strategy == 'heuristic':
-            # TODO: Adds a initialization scheme based on heuristic
+            # TODO: Adds a initialization scheme
             initial_pheromone = {}
 
         nx.set_edge_attributes(self.graph, initial_pheromone, name='pheromone')
 
-        self.num_nodes = len(self.nodes)
+        self.num_nodes = len(self.graph.nodes)
 
     def trace_pheromone(self, route: List[int], route_cost: float):
         """For a given route and its cost, deposit pheromone on all edges
 
         Args:
-            route (List[int]): [description]
-            route_cost (float): [description]
+            route (List[int]): Ant route
+            route_cost (float): Ant's route cost
         """
-        pheromone_deposit = self.pheromone_per_iteration / route_cost
+        pheromone_deposit = self.pheromone_amount / route_cost
 
         for nodes in range(self.num_nodes):
             idx1 = route[nodes]
@@ -92,11 +110,35 @@ class AntColony(BaseSolver):
 
     def evaporate(self):
         """
-        Applies evaporation strategy to all edges pheromones
+        Applies evaporation strategy to all edges pheromones and also to beta
         """
         for i in range(self.num_nodes):
             for j in range(i+1, self.num_nodes):
                 self.graph[i][j]['pheromone'] *= (1 - self.evaporation_rate)
+
+        self.beta *= (1 - self.beta_evaporation_rate)
+
+    def _evaluate_route(self, route: List[int]) -> Tuple[nx.Graph, float]:
+        evalutation_graph = nx.Graph()
+
+        # Transform route into steiner_graph
+        # TODO: Checks if add_path works with ant's routes
+        nx.add_path(evalutation_graph, route)
+
+        # Adds all edges from graph to steiner tree solution
+        conn_components = list(comp.connected_components(evalutation_graph))
+        while len(conn_components) > 1:
+            comp1 = conn_components[0]
+            comp2 = conn_components[1]
+            for j in range(0, len(comp1)):
+                for k in range(0, len(comp2)):
+                    if self.graph.has_edge(comp1[j], comp2[k]):
+                        evalutation_graph.add_edge(comp1[j], comp2[k])
+                        break
+            conn_components = list(comp.connected_components(evalutation_graph))
+
+        cycle = nx.find_cycle(evalutation_graph)
+        self.log.debug(f'Cycle found - {cycle}')
 
     def _solve(self) -> Tuple[nx.Graph, int]:
         """Solve Prize-Collecting Steiner Tree based on Ant Colony Optimization
@@ -104,37 +146,71 @@ class AntColony(BaseSolver):
         Returns:
             Tuple[nx.Graph, int]: Returns the Steiner Tree and its cost
         """
+        best_cost: float = float('inf')
+        best_route: List[int] = []
+        best_solution: nx.Graph = None
+        iterations_without_improvement = 0
 
         for ant in self.ants:
             ant.begin()
 
-        for generation in range(self.iterations):
+        for iteration in range(self.iterations):
+            self._iteration_start_time = time.time()
             # TODO: Is this the best strategy?
             # While the first ant hasn't reached the end, makes all turn
             while not self.ants[0].has_reached_end():
-                # For each ant, turn
+
+                # For each ant, move based on probability and updates pheromones
                 for ant in self.ants:
                     ant.turn()
 
+            # Applies evaporation strategy
             self.evaporate()
 
+            # Set a new begin for each ant
             for ant in self.ants:
                 ant.begin()
 
-            self.log.debug("Generation: %s - Cost: %s", generation, 0)
+            # Evaluates solution
+            evaluation_ant = Ant(self, 'evaluation_ant', update_pheromones=False)
+
+            solution, cost = self._evaluate_route(evaluation_ant.route)
+
+            if cost < best_cost:
+                best_cost = cost
+                best_route = evaluation_ant.route
+                best_solution = solution
+            else:
+                iterations_without_improvement += 1
+
+            if iterations_without_improvement > self.early_stopping:
+                break
+
+            self.log.debug("Generation: %s - Best Cost: %s - Best Route %s", iteration, best_cost, best_route)
+
+        self.steiner_tree, self.steiner_cost = best_solution, best_cost
+
+        return self.steiner_tree, self.steiner_cost
 
 
 class Ant():
-    def __init__(self, antcolony: AntColony, name: str):
+    def __init__(
+            self,
+            antcolony: AntColony,
+            name: str,
+            update_pheromones: bool = True
+    ):
         """
         Ant is an agent of Ant Colony Optimization Technique
 
         Args:
             antcolony (AntColony): AntColony where the current ant lives
             name (str): Name of the current ant
+            update_pheromones (bool, optional): If True the ant can deposit pheromones. Defaults to True
         """
         self.antcolony = antcolony
         self.name = name
+        self.update_pheromones = update_pheromones
 
         self.route: List[int] = []
         self.has_visited_all_nodes: bool = False
@@ -153,33 +229,63 @@ class Ant():
     def has_reached_end(self):
         """
         Indicates whether the ant has reached the end.
-
-        The ant reach its end when all nodes have been visited
         """
-        return self.has_visited_all_nodes
+        return self.has_visited_all_nodes or set(self.antcolony.terminals).issubset(set(self.route))
 
     def turn(self):
         """
         If the ant hasn't reached the end, chooses the next node.
+
+        The ant reach its end when all nodes have been visited or all terminals have been visited
         """
-        if not self.has_visited_all_nodes:
+        if not self.has_reached_end():
             self.move()
 
-            # If the Ant has visited all nodes
-            if len(self.route) == self.antcolony.dimension:
+            # TODO: Should pheromone deposits be made independently? Wouldn't it be better to rank solutions?
+            if len(self.route) == len(self.antcolony.graph.nodes):
                 self.has_visited_all_nodes = True
-                self.antcolony.trace_pheromone(self.antcolony._get_path_cost(self.route), self.route)
+
+                if self.update_pheromones:
+                    self.antcolony.trace_pheromone(self.antcolony._get_path_cost(self.route), self.route)
 
     def move(self):
         """
-        Chooses next node to visit
+        Chooses next node to visit based on probabilities. 
+
+        If p < p_choose_best, then the best path is chosen, otherwise it is selected
+        from a probability distribution weighted by the pheromone.
         """
-        for node in range(1, self.antcolony.dimension + 1):
-            if self.current_node == node or node in self.route:
+        current_neighbors = list(self.antcolony.graph.neighbors(self.current_node))
+        transition_probability = np.zeros(shape=(len(current_neighbors)))
+
+        def get_tau_and_eta(node: int) -> Tuple[float, float]:
+            distance = self.antcolony.graph[self.current_node][node]['cost']
+            eta = 1 / distance
+            tau = self.antcolony.graph[self.current_node][node]['pheromone']
+
+            return tau, eta
+
+        for i, node in enumerate(current_neighbors):
+            if node in self.route:
                 continue
 
-            if self.antcolony.edges[(node, self.current_node)] == 0:
+            # If the node has zero cost, move to it
+            if self.antcolony.graph[self.current_node][node]['cost'] == 0 and self.antcolony.graph[node]['prize'] >= 0:
                 self.route.append(node)
                 self.current_node = node
 
-        # TODO:
+                return
+
+            eta, tau = get_tau_and_eta(node)
+            transition_probability[i] = (tau**self.antcolony.alpha) * (eta**self.antcolony.beta)
+
+        transition_probability = transition_probability / np.sum(transition_probability)
+
+        # Given a transition probability vector, choose
+        if np.random.random() < self.antcolony.choose_best:
+            next_node = np.argmax(transition_probability)
+        else:
+            next_node = np.random.choice(current_neighbors, p=transition_probability)
+
+        self.route.append(next_node)
+        self.antcolony.log.debug("Ant %s is moving from %s to %s", self.name, self.current_node, next_node)
